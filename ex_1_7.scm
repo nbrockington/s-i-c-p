7d5fb3d29fd5770ca14f3d93582e8cf8affba56e
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-04-03 09:51:45 +0100

    Add files via upload

---
+++ ex_1_7.scm
@@ -0,0 +1,26 @@
+; An alternative strategy for implementing good-enough? is to watch
+; how guess changes from one iteration to the next and to stop when
+; the change is a very small fraction of the guess. Design a
+; square-root procedure that uses this kind of end test. Does this
+; work better for small and large numbers?
+
+(define (sqrt-iter new-guess old-guess x)
+   (if (good-enough? new-guess old-guess)
+       new-guess
+       (sqrt-iter (improve new-guess x) new-guess x)))
+
+
+(define (improve guess x)
+   (average guess (/ x guess)))
+
+
+(define (average x y)
+   (/ (+ x y) 2))
+
+
+(define (good-enough? new-guess old-guess)
+   (< (/ (abs (- old-guess new-guess)) old-guess) 0.001))
+
+
+(define (sqrti x)
+   (sqrt-iter 1 1.1 x))
