e27269fc917d028db9ec0d66a62534eaee4c1eba
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-04-03 09:25:57 +0100

    Add files via upload
    Exercise 1.3

---
+++ ex_1_3.scm
@@ -0,0 +1,6 @@
+; Exercise 1.3.  Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+
+(define (maxsq x y z)
+   (cond ((and (< z x) (< z y)) (+ (* x x) (* y y)) )
+         ((and (< y x) (< y z)) (+ (* x x) (* z z)) )
+         (else (+ (* y y) (* z z)))))
