171742ce4183c31c8820454113a6abdddc19268a
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-05-16 09:34:56 +0100

    Delete problem_01.scm

--- project_01_prisoner/problem_01.scm
+++
@@ -1,8 +0,0 @@
-
-
-(define (extract-entry game association-list)
-   (cond ((null? association-list)
-            (display "Error: This game play not found in list."))
-         ((equal? game (car (car association-list)))
-            (car association-list))
-         (else (extract-entry game (cdr association-list)))))
