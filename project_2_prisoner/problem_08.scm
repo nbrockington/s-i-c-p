025d5efd7e92e6425cd53c671cae79a2efd7bf8c
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-05-21 10:50:11 +0100

    Update problem_08.scm

--- project_2_prisoner/problem_08.scm
+++ project_2_prisoner/problem_08.scm
@@ -5,3 +5,12 @@
     (if	(and (string=? (strat my-history other-history) "d")
       	     (>	(random	1.0) gentleness-factor))
       	"d" "c")))
+
+
+; Procedure SLIGHTLY-GENTLE-NASTY:
+
+(define SLIGHTLY-GENTLE-NASTY (GENTLE NASTY 0.1))
+
+; Procedure SLIGHT-GENTLE-EYE-FOR-EYE:
+
+(define SLIGHTLY-GENTLE-EYE-FOR-EYE (GENTLE EYE-FOR-EYE 0.1))
