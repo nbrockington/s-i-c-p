92aa8133c8981dfadee97721f3cad3b285080683
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-05-26 16:45:30 +0100

    Update ex_2_17.scm

--- chapter_2/ex_2_17.scm
+++ chapter_2/ex_2_17.scm
@@ -1,7 +1,7 @@
 ; Define a procedure last-pair that returns the list that contains
 ; only the last element of a given (nonempty) list:
 
-(define (last-pair list)
-  (if (= (length list) 1)
-      (car list)
-      (last-pair (cdr list))))
+(define (last-pair items)
+  (if (= (length items) 1)
+      (car items)
+      (last-pair (cdr items))))
