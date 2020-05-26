a048a0f2a6972acfb59a522cfc3d4e56c9963798
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-05-26 14:58:08 +0100

    Create ex_2_18.scm

---
+++ chapter_2/ex_2_18.scm
@@ -0,0 +1,11 @@
+; Define a procedure reverse that takes a list as argument and returns                
+; a list of the same elements in reverse order:                                       
+
+(define	(reverse list)
+  (define (reverse-helper from-list to-list)
+    (if	(= (length from-list) 0)
+      	to-list
+      	(reverse-helper (cdr from-list) (cons (car from-list) to-list))))
+  (reverse-helper list '()))
+  
+  
