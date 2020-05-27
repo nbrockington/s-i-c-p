54a17051a21598ffd171e555c9788c911c61ab95
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-05-27 13:37:01 +0100

    Exercise 2.30

---
+++ chapter_2/ex_2_30.scm
@@ -0,0 +1,26 @@
+; Define a procedure square-tree analogous to the square-list
+; procedure of exercise 2.21. That is, square-tree should behave as
+; follows:
+
+; (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+; 
+; (1 (4 (9 16) 25) (36 49))
+
+; Define square-tree both directly (i.e., without using any
+; higher-order procedures) and also by using map and recursion.
+
+
+(define (square-tree-direct tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree-direct (car tree))
+                    (square-tree-direct (cdr tree))))))
+
+
+(define (square-tree-map tree)
+  (map (lambda (subtree)
+         (if (pair? subtree)
+             (square-tree-map subtree)
+             (* subtree subtree)))
+       tree))
+
