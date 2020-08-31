707fa0fcb21cac017790ab5120e0fcd4b2de660b
Nela Brockington <49438664+nbrockington@users.noreply.github.com>
2020-08-31 15:21:26 +0100

    Exercise 3.1 (accumulator generator)

---
+++ chapter_3/ex_3_01.scm
@@ -0,0 +1,18 @@
+; An accumulator is a procedure that is called repeatedly with a
+; single numeric argument and accumulates its arguments into a
+; sum. Each time it is called, it returns the currently accumulated
+; sum. Write a procedure make-accumulator that generates accumulators,
+; each maintaining an independent sum. The input to make-accumulator
+; should specify the initial value of the sum; for example:
+;
+;(define A (make-accumulator 5))
+;(A 10)
+;15
+;(A 10)
+;25
+
+
+(define (make-accumulator acc)
+  (lambda (amount) 
+    (begin (set! acc (+ acc amount))
+	   acc)))
